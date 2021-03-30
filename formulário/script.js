9725e22a28c18728bd62550ea8faa0087a0ebe39
João Guilherme Mendes <jmgvh9000@gmail.com>
2021-03-30 06:25:24 -0300

    formulario

---
+++ formulário/script.js
@@ -0,0 +1,23 @@
+function cadastrar() {
+    
+    
+    alert('oi')
+    var nome1 = document.getElementById('nome1').textContent;
+    var email1 = document.getElementById('email1')
+    var tel1 = document.getElementById('tel1')
+    var genero = document.getElementById('info1')
+    //var res = document.getElementById('res')
+
+    res.innerHTML = nome1
+    alert(nome1)
+    //alert(email1)
+    //alert(tel1)
+    //alert(genero)
+    //alert(res1)
+
+    //if (nome.value == 0 || email.value == 0 || tel.value == 0 || genero.value == 0) {
+    //    res.innerText = '[ERRO] Informações Erradas'}
+        
+
+    
+}
